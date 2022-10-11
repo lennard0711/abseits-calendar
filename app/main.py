39fc327f1eb5f4f30da8dd64c6aa699679aac4f5
Lennard Indlekofer <info@lennard0711.eu>
2022-10-11 02:53:10 +0200

    "fixed" table encoding

--- app/main.py
+++ app/main.py
@@ -16,7 +16,8 @@ caldav_url = os.environ.get('CALDAV_URL')
 caldav_user = os.environ.get('CALDAV_USER')
 caldav_password = os.environ.get('CALDAV_PASSWORD')
 # Set abseits.biz vars
-abseits_url = os.environ.get('ABSEITS_URL')
+abseits_uid = os.environ.get('ABSEITS_UID')
+abseits_kader = os.environ.get('ABSEITS_KADER')
 abseits_user = os.environ.get('ABSEITS_USER')
 abseits_password = os.environ.get('ABSEITS_PASSWORD')
 
@@ -38,27 +39,16 @@ def main():
         ('rs', 'GetSpieleTableFullLayer'),
         ('rst', ''),
         ('rsrnd', '1665444178344'),
-        ('rsargs[]', '400000000205'),
-        ('rsargs[]', 'BZL'),
+        ('rsargs[]', abseits_uid),
+        ('rsargs[]', abseits_kader),
         ('rsargs[]', '0'),
         ('rsargs[]', ''),
         ('rsargs[]', ''),
     ])
-    assert resp.status_code == 200
 
-
-
-    # get table
-    page = session.get(abseits_url)
-
-    parsed = bs4.BeautifulSoup(page.text, "lxml")
-    container = parsed.find("div", {"id": "FullLayer400000000205"})
-    table = container.find("table")
-
-    table_str = str(table)
-    a = pandas.read_html(table_str)[0]
-
-    #print(a)
+    raw = get_full_table.text[50:-9].replace("\\", "")
+    dirty_table = pandas.read_html(raw)[0]
+    clean_table = dirty_table.to_string().replace("u00e4", "ä").replace("u00c4", "Ä").replace("u00fc", "ü").replace("u00dc", "Ü").replace("u00f6", "ö").replace("u00d6", "Ö").replace("u00df", "ß")
 
     # Initiate DAVClient object
     client = caldav.DAVClient(url=caldav_url, username=caldav_user, password=caldav_password)
@@ -81,9 +71,6 @@ def main():
         my_calendar = my_principal.calendar(name=os.environ.get('CALDAV_CALENDAR'))
 
     all_events = my_calendar.events()
-
-    print("test")
-
 
 if __name__ == '__main__':
     main()
